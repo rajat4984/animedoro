8dfb81daac3026ecec8ce84960f02d40222b17d7
rajat4984 <rs20021023@gmail.com>
2023-06-16 01:42:44 +0530

    timer completed

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -2,7 +2,26 @@ import React from "react";
 import "./timer.scss";
 
 function Timer() {
-  return <div> </div>;
+  return (
+    <div className="timer">
+      <div className="circle">
+        <svg viewBox="0 0 220 220" xmlns="http://www.w3.org/2000/svg">
+          <g transform="translate(110,110)">
+            <circle r="100" class="e-c-base" />
+            <g transform="rotate(-90)">
+              <circle r="100" class="e-c-progress" />
+              <g id="e-pointer">
+                <circle cx="100" cy="0" r="8" class="e-c-pointer" />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+      <div class="controlls">
+        <div class="display-remain-time">00:30</div>
+      </div>
+    </div>
+  );
 }
 
 export default Timer;
