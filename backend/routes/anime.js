0b7e589d40f60f37d8fa0a1de7bde33e4746ee49
rajat4984 <rs20021023@gmail.com>
2023-07-15 23:53:38 +0530

    route changed

--- backend/routes/anime.js
+++ backend/routes/anime.js
@@ -3,8 +3,8 @@ const axios = require("axios");
 
 router.get("/get-anime-list",async(req,res)=>{
     try {
-        const response = await axios("https://api.myanimelist.net/v2/anime?q=one&limit=4",{headers:{Authorization:`Bearer ${req.query.access_token}`}});
-        res.status(200).json(response);
+        const response = await axios.get("https://api.myanimelist.net/v2/anime?q=one&limit=4",{headers:{Authorization:`Bearer ${req.query.access_token}`}});
+        res.status(200).json(response.data);
     } catch (error) {
         res.status(500).json(error);
     }
